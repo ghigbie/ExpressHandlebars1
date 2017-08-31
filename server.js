38ff5d697b3c5d8ed16a8ef770699b0c401f45ba
George Higbie <gehigbie@gmail.com>
2017-08-31 14:03:00 +0000

    Add more routes

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ app.get('/', (req, res) => {
     res.send('<h1> Yo puppy!</h1>');
 });
 
+app.get('/about', (req, res) => {
+    res.send('<h1>About page</h1>'); 
+});
+
+app.get('/bad', (req, res) =>{
+    res.send('<h1>Page Not Found!</h1>');
+});
+
+app.get('*', (req, res) => {
+    res.send('<h1>Page Not Found!</h1>');
+});
+
 app.listen(PORT, IP, ()=> {
    console.log(`The server is listening on ${PORT}`);
 });
