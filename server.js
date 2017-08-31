a6f1d883219ca25cb0ae884a729d5dc40d6a0271
George Higbie <gehigbie@gmail.com>
2017-08-31 13:47:26 +0000

    Configure first get route

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 let PORT = process.env.PORT;
 let IP = process.env.IP;
 
+app.get('/', (req, res) => {
+    res.send('<h1> Yo puppy!</h1>');
+});
+
 app.listen(PORT, IP, ()=> {
    console.log(`The server is listening on ${PORT}`);
 });
